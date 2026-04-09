74542c2b4fb3da286c0aca13e5ca04958baf9f82
aegis945 <artiom.lekincev@gmail.com>
2026-04-09 15:39:48 +0300

    add example .env file and update settings

--- config/settings.py
+++ config/settings.py
@@ -8,9 +8,9 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-local-trading-journal-key-change-in-prod')
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
